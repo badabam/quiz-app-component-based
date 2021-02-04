27f1e772ae1bf4cff01c53818301df9fc988bb68
Jeremias Erbs <jerry@badabam.de>
2021-02-04 14:28:21 +0100

    Create Card component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import Button from './components/Button'
+import Header from './components/Header'
+
+const header = Header('Quiz App', 'May the best win!')
+document.body.append(header)
 
 const button = Button('Click me')
 const button2 = Button('Hold me')
