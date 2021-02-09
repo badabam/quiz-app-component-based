f2f2beded24317b76c0a672222f8e827c4b4fec1
Jeremias Erbs <jerry@badabam.de>
2021-02-09 18:19:10 +0100

    Merging stash

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
-import CreateForm from './components/CreateForm'
+import Card from './components/Card'
+import CreatePage from './components/CreatePage'
+import Grid from './components/Grid/Grid'
 import Header from './components/Header'
 import HomePage from './components/HomePage/HomePage'
 import Navigation from './components/Navigation/Navigation'
-import createElement from './lib/createElement'
 
 const cards = []
 
 const header = Header('Quiz App')
-
 const navigation = Navigation(onNavigate)
-
 const homePage = HomePage()
+const createPage = CreatePage(onSubmit, true)
 
-const form = CreateForm(onSubmit)
-
-const createPage = createElement(
-  'main',
-  { className: 'CreatePage', hidden: true },
-  form
-)
-
-const grid = createElement(
-  'div',
-  { className: 'appGrid' },
-  header,
-  homePage,
-  createPage,
-  navigation
-)
-
+const grid = Grid(header, homePage, createPage, navigation)
 document.body.append(grid)
 
 function onSubmit(question, answer) {
@@ -40,7 +24,7 @@ function onNavigate(text) {
   if (text === 'Home') {
     homePage.show()
     createPage.hide()
-    header.setText('Homepage')
+    header.setText('Quiz App')
   }
 
   if (text === 'Create') {
