c8a0768b6449b965b20202bdaf2b6e79df00ad47
Jeremias Erbs <jerry@badabam.de>
2021-02-09 18:14:37 +0100

    Small improvements

--- src/components/HomePage/HomePage.stories.js
+++ src/components/HomePage/HomePage.stories.js
@@ -1,12 +1,16 @@
 import HomePage from './HomePage'
 
 export default {
-  title: 'HomePage',
+  title: 'Pages/HomePage',
   component: HomePage,
 }
 
 export const base = () =>
   HomePage([
+    {
+      question: 'This could be your question',
+      answer: 'This could be your answer',
+    },
     {
       question: 'This could be your question',
       answer: 'This could be your answer',
