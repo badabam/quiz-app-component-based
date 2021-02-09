ee1ac4049f315009ae2f06c16314b140c8914b80
Jeremias Erbs <jerry@badabam.de>
2021-02-09 14:31:14 +0100

    Improve Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,25 @@
 import createElement from '../../lib/createElement'
+import homeSvg from '@fortawesome/fontawesome-free/svgs/solid/home.svg'
+import bookmarkSvg from '@fortawesome/fontawesome-free/svgs/solid/bookmark.svg'
+import createSvg from '@fortawesome/fontawesome-free/svgs/solid/edit.svg'
+import settingsSvg from '@fortawesome/fontawesome-free/svgs/solid/cog.svg'
 import './Navigation.css'
 
-import homeSVG from '@fortawesome/fontawesome-free/svgs/solid/home.svg'
-import bookmarkSVG from '@fortawesome/fontawesome-free/svgs/solid/bookmark.svg'
-import createSVG from '@fortawesome/fontawesome-free/svgs/solid/plus-square.svg'
-import settingsSVG from '@fortawesome/fontawesome-free/svgs/solid/cog.svg'
-
 const buttonsConfig = [
-  { svgPath: homeSVG, text: 'Home' },
-  { svgPath: bookmarkSVG, text: 'Bookmarks' },
-  { svgPath: createSVG, text: 'Create' },
-  { svgPath: settingsSVG, text: 'Settings' },
+  { text: 'Home', svgPath: homeSvg },
+  { text: 'Bookmarks', svgPath: bookmarkSvg },
+  { text: 'Create', svgPath: createSvg },
+  { text: 'Settings', svgPath: settingsSvg },
 ]
 
 export default function Navigation(onNavigate) {
   const buttons = buttonsConfig.map(({ svgPath, text }) => {
-    const button = createElement('img', { src: svgPath, alt: text })
+    const button = createElement(
+      'button',
+      { className: 'Navigation__button' },
+      createElement('img', { src: svgPath, alt: '' }),
+      text
+    )
     button.addEventListener('click', () => onNavigate(text))
     return button
   })
