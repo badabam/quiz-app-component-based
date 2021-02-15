3bd86578548195db16d062a78a7024d2ce5d436c
Jeremias Erbs <jerry@badabam.de>
2021-02-15 10:33:36 +0100

    Change home icon to user.svg

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import createElement from '../../lib/createElement'
-import homeSvg from '@fortawesome/fontawesome-free/svgs/solid/home.svg'
+import homeSvg from '@fortawesome/fontawesome-free/svgs/solid/user.svg'
 import bookmarkSvg from '@fortawesome/fontawesome-free/svgs/solid/bookmark.svg'
 import createSvg from '@fortawesome/fontawesome-free/svgs/solid/edit.svg'
 import settingsSvg from '@fortawesome/fontawesome-free/svgs/solid/cog.svg'
